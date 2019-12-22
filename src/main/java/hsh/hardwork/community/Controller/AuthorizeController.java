5bb6ed06b3af21c1ad849a583027078f077ae257
djyd666 <837025268@qq.com>
2019-12-22 21:23:46 +0800

    add check cookies

--- src/main/java/hsh/hardwork/community/Controller/AuthorizeController.java
+++ src/main/java/hsh/hardwork/community/Controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package hsh.hardwork.community.Controller;
 
 import hsh.hardwork.community.dto.AccessTokenDto;
 import hsh.hardwork.community.dto.GithubUser;
+import hsh.hardwork.community.mapper.UserMapper;
+import hsh.hardwork.community.model.User;
 import hsh.hardwork.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,10 +11,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -21,24 +31,44 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+
     /*
     与github用户登陆授权对应的接口，自己可以定义
      */
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name= "state") String state){
+                           @RequestParam(name= "state") String state,
+                           HttpServletResponse response){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setCode(code);
         accessTokenDto.setRedirct_uri(redirectUri);
         accessTokenDto.setState(state);
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
-
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getId());
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        //System.out.println(user.getName());
+        if(githubUser!=null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            System.out.println("___________________-");
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));  //自己设置cookie的key和value并存入数据库
+            //request.getSession().setAttribute("user",githubUser);//登陆成功，写cookie和session
+            return "redirect:/";//重新定向到主页
+        }else {
+            return "redirect:/";
 
-        return "index";
+
+        }
+
+
     }
 
 }
--- src/main/java/hsh/hardwork/community/Controller/IndexController.java
+++ src/main/java/hsh/hardwork/community/Controller/IndexController.java
@@ -1,12 +1,35 @@
 package hsh.hardwork.community.Controller;
 
+import hsh.hardwork.community.mapper.UserMapper;
+import hsh.hardwork.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String indexController(){
+    public String indexController(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies(); //获取浏览器传来的cookie值
+        for (Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){ //在数据库中进行查找是否有对应cookie
+                String token =cookie.getValue();
+                User user=userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 }
